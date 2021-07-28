6f3912786e227c8bd3cf88a12e0ba809b32914ba
roach <roach@cleaninglab.co.kr>
2021-07-28 23:16:55 +0900

    [Chapter01] - [Example01] refactor: 가격을 정하는 함수를 별도의 함수로 분리하였음
    하나의 로직으로 판단되어, 함수를 분리하였습니다.

--- Chapter01/Example01/Example01.js
+++ Chapter01/Example01/Example01.js
@@ -11,27 +11,8 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf.audience, play.type);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -48,6 +29,33 @@ function statement(invoice, plays) {
   result += `총액 ${format(totalAmount / 100)}\n`;
   result += `적립 포인트 ${volumeCredits}점`;
   return result;
+}
+
+// 값이 바뀌지 않는 변수는 매개변수로 전달
+function amountFor(audience, type) {
+  let thisAmount = 0;
+
+  switch (type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (audience > 30) {
+        thisAmount += 1000 * (audience - 30);
+      }
+      break;
+
+    case "comedy":
+      thisAmount = 30000;
+      if (audience > 20) {
+        thisAmount += 10000 + 500 * (audience - 20);
+      }
+      thisAmount += 300 * audience;
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르: ${type}`);
+  }
+
+  return thisAmount;
 }
 
 export default statement;
