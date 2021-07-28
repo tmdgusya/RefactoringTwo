c09fdddb4ebfe950b487f1e8fe0778f5a15bd25f
roach <roach@cleaninglab.co.kr>
2021-07-29 00:29:30 +0900

    [Chpater01] - [Example01] refactor: 중첩함수로 개선
    내부 로직에서만 사용되는 것들은 private 처럼 관리될 필요가 있음, usd 는 충분히 내부로직이 아닌 util 로써 관리될 수도 있다고 생각해서 내부 로직으로 넣지 않았음.
    
    스코프내 변수를 활용할 수 있는 것들은 활용할 수 있도록 리팩토링 함.

--- Chapter01/Example01/Example01.js
+++ Chapter01/Example01/Example01.js
@@ -4,7 +4,7 @@ function statement(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let ticket of invoice.tickets) {
-    const play = findPlayFromPlayList(ticket, plays);
+    const play = findPlayFromPlayList(ticket.playID);
     let thisAmount = amountFor(ticket.audience, play.type);
 
     volumeCredits += volumeCreditsFor(ticket.audience, play.type);
@@ -16,6 +16,56 @@ function statement(invoice, plays) {
   result += `총액 ${usd(totalAmount / 100)}\n`;
   result += `적립 포인트 ${volumeCredits}점`;
   return result;
+
+  function volumeCreditsFor(audience, type) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(audience - 30, 0);
+
+    volumeCredits += bonusFeePolicy(audience, type);
+
+    return volumeCredits;
+  }
+
+  function bonusFeePolicy(audience, type) {
+    let bonusFee = 0;
+
+    if ("comedy" === type) {
+      bonusFee = Math.floor(audience / 5);
+    }
+
+    return bonusFee;
+  }
+
+  function findPlayFromPlayList(id) {
+    return plays[id];
+  }
+
+  // 값이 바뀌지 않는 변수는 매개변수로 전달
+  function amountFor(audience, type) {
+    let thisAmount = 0;
+
+    switch (type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (audience > 30) {
+          thisAmount += 1000 * (audience - 30);
+        }
+        break;
+
+      case "comedy":
+        thisAmount = 30000;
+        if (audience > 20) {
+          thisAmount += 10000 + 500 * (audience - 20);
+        }
+        thisAmount += 300 * audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${type}`);
+    }
+
+    return thisAmount;
+  }
 }
 
 function usd(money) {
@@ -24,56 +74,6 @@ function usd(money) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(money / 100);
-}
-
-function volumeCreditsFor(audience, type) {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(audience - 30, 0);
-
-  volumeCredits += bonusFeePolicy(audience, type);
-
-  return volumeCredits;
-}
-
-function bonusFeePolicy(audience, type) {
-  let bonusFee = 0;
-
-  if ("comedy" === type) {
-    bonusFee = Math.floor(audience / 5);
-  }
-
-  return bonusFee;
-}
-
-function findPlayFromPlayList(performance, plays) {
-  return plays[performance.playID];
-}
-
-// 값이 바뀌지 않는 변수는 매개변수로 전달
-function amountFor(audience, type) {
-  let thisAmount = 0;
-
-  switch (type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (audience > 30) {
-        thisAmount += 1000 * (audience - 30);
-      }
-      break;
-
-    case "comedy":
-      thisAmount = 30000;
-      if (audience > 20) {
-        thisAmount += 10000 + 500 * (audience - 20);
-      }
-      thisAmount += 300 * audience;
-      break;
-
-    default:
-      throw new Error(`알 수 없는 장르: ${type}`);
-  }
-
-  return thisAmount;
 }
 
 export default statement;
