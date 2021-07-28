d0ab91387d4bb4c00982908fe31d142e8e4cd65e
roach <roach@cleaninglab.co.kr>
2021-07-28 20:51:13 +0900

    [Chapter01] - [Example01] 테스트 코드 작성
    함수가 분리되어 있지 않아 테스트 작성이 엄청나게 힘들다..
    함수를 빠르게 분리해야겠다는 생각이 먼져든다.

---
+++ Chapter01/Example01.spec.js
@@ -0,0 +1,83 @@
+import playJsonData from "./plays.json";
+import invoiceJsonData from "./invoices.json";
+import flaseJsonData from "./falsePlay.json";
+import tragedyDefaultPriceJson from "./invoices_as_tragedy_30.json";
+import comedyDefaultPriceJson from "./invoices_as_like_under_30.json";
+import { expect, describe, it } from "@jest/globals";
+import statement from "./Example01";
+
+const testPlaysJsonFile = JSON.parse(JSON.stringify(playJsonData));
+const testInvoiceJsonData = JSON.parse(JSON.stringify(invoiceJsonData));
+
+describe("statement() result test", () => {
+  it("USER 의 청구내역명이 RESULT 로 정상적으로 출력되는지 확인한다.", () => {
+    const billingDetail = statement(invoiceJsonData[0], playJsonData);
+
+    const expectMessage = `청구 내역 (고객명: BigCo)
+hemlet : $6,500.00 (55석)
+As You Like It : $5,800.00 (35석)
+Othello : $5,000.00 (40석)
+총액 $0.00
+적립 포인트 47점`;
+
+    expect(billingDetail).toEqual(expectMessage);
+  });
+
+  it("USER가 비극 티켓을 30석 이하일때 사려할 시 사려할시 40000원으로 계산되는지 확인한다.", () => {
+    const billingDetail = statement(tragedyDefaultPriceJson[0], playJsonData);
+
+    const expectMessage = `청구 내역 (고객명: Roach)
+hemlet : $4,000.00 (30석)
+As You Like It : $5,800.00 (35석)
+Othello : $4,000.00 (30석)
+총액 $0.00
+적립 포인트 12점`;
+
+    expect(billingDetail).toEqual(expectMessage);
+  });
+
+  it("USER가 희극 티켓을 20석 이하일때 사려할 시 사려할시 30000원으로 계산되는지 확인한다.", () => {
+    const billingDetail = statement(comedyDefaultPriceJson[0], playJsonData);
+
+    const expectMessage = `청구 내역 (고객명: BigCo)
+hemlet : $6,500.00 (55석)
+As You Like It : $3,600.00 (20석)
+Othello : $5,000.00 (40석)
+총액 $0.00
+적립 포인트 39점`;
+
+    expect(billingDetail).toEqual(expectMessage);
+  });
+
+  it("없는 카테고리의 티켓을 사려할시 Error 가 나는지 확인한다. 또한 에러메세지가 정상적으로 출력되는지 확인한단.", () => {
+    const billingDetail = statement(comedyDefaultPriceJson[0], playJsonData);
+
+    const expectMessage = `청구 내역 (고객명: BigCo)
+hemlet : $6,500.00 (55석)
+As You Like It : $3,600.00 (20석)
+Othello : $5,000.00 (40석)
+총액 $0.00
+적립 포인트 39점`;
+
+    expect(billingDetail).toEqual(expectMessage);
+  });
+
+  it("보너스 점수를 정확히 계산하는지 확인한다.", () => {
+    const billingDetail = statement(comedyDefaultPriceJson[0], playJsonData);
+
+    const expectMessage = `청구 내역 (고객명: BigCo)
+hemlet : $6,500.00 (55석)
+As You Like It : $3,600.00 (20석)
+Othello : $5,000.00 (40석)
+총액 $0.00
+적립 포인트 39점`;
+
+    expect(billingDetail).toEqual(expectMessage);
+  });
+
+  it("알수없는 장로를 고를시, 에러를 리턴하는지 확인한다.", () => {
+    expect(() => {
+      statement(comedyDefaultPriceJson[0], flaseJsonData);
+    }).toThrowError(new Error("알 수 없는 장르: movie"));
+  });
+});
