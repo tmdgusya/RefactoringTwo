950676b0372579409af80430ce1a82928037afda
roach <roach@cleaninglab.co.kr>
2021-07-29 21:41:29 +0900

    [Chapter01] - [Example01] refactor: renderText 와 statement 함수를 분리

--- Chapter01/Example01/Example01.js
+++ Chapter01/Example01/Example01.js
@@ -1,4 +1,8 @@
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let ticket of invoice.tickets) {
